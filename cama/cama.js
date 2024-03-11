16a1b42e2656a7247a27bf6796db4937d9e1216f
Rian <114163647+Rian2009@users.noreply.github.com>
2024-03-11 19:13:01 -0300

    Add files via upload

---
+++ cama/cama.js
@@ -0,0 +1,36 @@
+img = "";
+status = "";
+
+function preload()
+{
+    img = loadImage("./fotos/cama.avif");
+}
+
+function setup()
+{
+    canvas = createCanvas(400, 300);
+    canvas.position(windowWidth/2 -200, windowHeight/2 -100);
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detectando Objetos";
+}
+
+function modelLoaded()
+{
+    console.log("Modelo Carregado!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function draw(){
+    image(img, 0, 0, 400, 300);
+}
+
+
+function gotResult(error,results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results); 
+}
